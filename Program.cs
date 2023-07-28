8edc02146cfa117705959a68e9dcaebc56e746e8
Aditya Khatry <adityak@lims1.com>
2023-07-28 16:55:10 +1000

    Adding the while loop to ask the player to play again

--- Program.cs
+++ Program.cs
@@ -7,80 +7,121 @@ namespace ROCKPAPERSCISSORS
         {
             string inputPlayer, inputCPU;
             int randomInt;
-            int scorePlayer = 0;
-            int scoreCPU = 0;
-
-            Console.WriteLine("Choose between ROCK, PAPER and SCISSORS:  ");
-            inputPlayer = Console.ReadLine();
-
-            Random rnd = new Random();
-
-            randomInt = rnd.Next(1, 4);
 
 
-            switch (randomInt)
+            bool playAgain = true;
+            
+            while(playAgain)
             {
-                case 1:
-                    inputCPU = "ROCK";
-                    Console.WriteLine("Computer chose ROCK.");
-                    if (inputPlayer == "ROCK")
-                    {
-                        Console.WriteLine("DRAW!!!\n\n");
-                    }
-                    else if (inputPlayer == "Paper")
-                    {
-                        Console.WriteLine("Player WINS!!\n\n");
-                        scorePlayer++;
-                    }
-                    else if (inputPlayer == "Scissors")
-                    {
-                        Console.WriteLine("Player LOSES!!\n\n");
-                        scoreCPU++;
-                    }
-                    break;
+                int scorePlayer = 0;
+                int scoreCPU = 0;
 
-                case 2:
-                    inputCPU = "PAPER";
-                    Console.WriteLine("Computer chose PAPER.");
-                    if (inputPlayer == "ROCK")
+                while (scorePlayer < 3 && scoreCPU < 3)
+                {
+                    Console.WriteLine("Choose between ROCK, PAPER and SCISSORS:  ");
+                    inputPlayer = Console.ReadLine();
+
+                    Random rnd = new Random();
+
+                    randomInt = rnd.Next(1, 4);
+
+
+                    switch (randomInt)
                     {
-                        Console.WriteLine("Player LOSES!!\n\n");
-                        scoreCPU++;
+                        case 1:
+                            inputCPU = "ROCK";
+                            Console.WriteLine("Computer chose ROCK.");
+                            if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("DRAW!!!\n\n");
+                            }
+                            else if (inputPlayer == "Paper")
+                            {
+                                Console.WriteLine("Player WINS!!\n\n");
+                                scorePlayer++;
+                            }
+                            else if (inputPlayer == "Scissors")
+                            {
+                                Console.WriteLine("Player LOSES!!\n\n");
+                                scoreCPU++;
+                            }
+                            break;
+
+                        case 2:
+                            inputCPU = "PAPER";
+                            Console.WriteLine("Computer chose PAPER.");
+                            if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("Player LOSES!!\n\n");
+                                scoreCPU++;
+                            }
+                            else if (inputPlayer == "Paper")
+                            {
+                                Console.WriteLine("DRAW!!\n\n");
+
+                            }
+                            else if (inputPlayer == "Scissors")
+                            {
+                                Console.WriteLine("Player WINS!!\n\n");
+                                scorePlayer++;
+
+                            }
+                            break;
+
+                        case 3:
+                            inputCPU = "SCISSORS";
+                            Console.WriteLine("Computer chose SCISSORS.");
+                            if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("Player WINS!!\n\n");
+                                scorePlayer++;
+
+                            }
+                            else if (inputPlayer == "Paper")
+                            {
+                                Console.WriteLine("Player LOSES!!\n\n");
+                            }
+                            else if (inputPlayer == "Scissors")
+                            {
+                                Console.WriteLine("DRAW!!\n\n");
+                                scoreCPU++;
+                            }
+                            break;
+                        default:
+                            break;
                     }
-                    else if (inputPlayer == "Paper")
+
+                    if (scorePlayer == 3)
                     {
-                        Console.WriteLine("DRAW!!\n\n");
+                        Console.WriteLine("Player WON!");
+                    }
+
+                    else if (scoreCPU == 3)
+                    {
+                        Console.WriteLine("CPU WON!");
+                    }
+
+                    else
+                    {
 
                     }
-                    else if (inputPlayer == "Scissors")
-                    {
-                        Console.WriteLine("Player WINS!!\n\n");
-                        scorePlayer++;
+                }
+            }      
 
-                    }
-                    break;
-
-                case 3:
-                    inputCPU = "SCISSORS";
-                    Console.WriteLine("Computer chose SCISSORS.");
-                    if (inputPlayer == "ROCK")
-                    {
-                        Console.WriteLine("Player WINS!!\n\n");
-                        scorePlayer++;
-
-                    }
-                    else if (inputPlayer == "Paper")
-                    {
-                        Console.WriteLine("Player LOSES!!\n\n");
-                    }
-                    else if (inputPlayer == "Scissors")
-                    {
-                        Console.WriteLine("DRAW!!\n\n");
-                        scoreCPU++;
-                    }
-                    break;
-                default:
-                    break;
+            Console.WriteLine("Do you want to play again? (y/n)");
+            string loop = Console.ReadLine();
+            if (loop == "y")
+            {
+                playAgain = true;
+                Console.Clear();
+            }
+            else if (loop == "n")
+            {
+                playAgain = false;
+            }
+            else
+            {
+                
             }
         }
     }
